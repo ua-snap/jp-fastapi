e2bcfa8f520670b7fef6dfa116617e267eb34707
Josh Paul <99696041+Joshdpaul@users.noreply.github.com>
2025-01-29 12:34:37 -0900

    Add files via upload

---
+++ jp-fastapi/app.py
@@ -0,0 +1,185 @@
+from typing import Annotated, Literal, Optional
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, model_validator, field_validator, conint, confloat
+from pydantic_core.core_schema import FieldValidationInfo
+
+
+# local
+from util import (
+    mockup_message,
+    validate_parameters_against_catalog,
+    fetch_data_using_catalog,
+    package_data,
+)
+
+app = FastAPI()
+
+# The pydantic models below are used to validate the query parameters for each route.
+# They check for sane values for each parameter, and provide default values where appropriate.
+# They only check that the request is well-formed.
+# The lists used in the models could come directly from the metadata catalog and be populated programmatically.
+# *** They do not check for data availability! ***
+# Data availability is checked within each route using the pre-validated parameters from these base models.
+
+
+class AboutParameters(BaseModel):
+    model_config = {"extra": "forbid"}
+    variable: Literal["t2", "pr", "clt"] | None = None
+
+
+class AtmosphereDataParameters(BaseModel):
+    # model_config = {"extra": "forbid"}
+    variable: Literal["t2", "t10", "clt", "dw"] = ...
+    source: Literal["cmip5", "cmip6", "cmip7"] = ...
+    lat: confloat(ge=50, le=90) = ...
+    lon: confloat(ge=-180, le=180) = ...
+    start_year: conint(ge=1950, le=2100) = ...
+    end_year: conint(ge=1950, le=2100) = ...
+    format: Literal["json", "csv", "netcdf", "geotiff"] = "json"
+
+
+class HydrosphereDataParameters(BaseModel):
+    model_config = {"extra": "forbid"}
+    variable: Literal["pr", "swe", "rx1day", "rx5day"] = ...
+    # source: Literal["cmip5", "cmip6", "cmip7"] = ...
+    source: list[str] = ...
+    # lat: confloat(ge=50, le=90) = ...
+    # lon: confloat(ge=-180, le=180) = ...
+    location: Literal["AK1", "AK2", "AK3"] | None = None
+    lat: confloat(ge=50, le=90) | None = None
+    lon: confloat(ge=-180, le=180) | None = None
+    start_year: conint(ge=1950, le=2100) = ...
+    end_year: conint(ge=1950, le=2100) = ...
+    format: Literal["json", "csv", "netcdf", "geotiff"] = "json"
+
+    @field_validator("source")
+    def validate_option(cls, v):
+        allowable_sources = ["cmip5", "cmip6", "cmip7"]
+        if not all(item in allowable_sources for item in v):
+            raise ValueError(f"Source must be in {allowable_sources}.")
+        return v
+
+    @model_validator(mode="after")
+    def validate_years(self):
+        if not self.start_year < self.end_year:
+            raise ValueError("Start year must be less than end year.")
+        return self
+
+    @model_validator(mode="after")
+    def validate_lat_lon(self):
+        if self.location is None and self.lat is None:
+            raise ValueError("Either location or both lat & lon must be provided.")
+        if self.location is None and self.lon is None:
+            raise ValueError("Either location or both lat & lon must be provided.")
+        if self.location is not None and (self.lat is not None or self.lon is not None):
+            raise ValueError(
+                "Can only request location or lat & lon, cannot request both."
+            )
+        return self
+
+
+class BiosphereDataParameters(BaseModel):
+    model_config = {"extra": "forbid"}
+    variable: Literal["flam", "veg", "beetles"] = ...
+    source: Literal["era5", "cmip5"] = ...
+    lat: confloat(ge=50, le=90) = ...
+    lon: confloat(ge=-180, le=180) = ...
+    start_year: conint(ge=1950, le=2100) = ...
+    end_year: conint(ge=1950, le=2100) = ...
+    format: Literal["json", "csv", "netcdf", "geotiff"] = "json"
+
+
+class CryosphereDataParameters(BaseModel):
+    model_config = {"extra": "forbid"}
+    variable: Literal["siconc", "slie"] = ...
+    source: Literal["cmip5", "cmip6", "cmip7"] = ...
+    lat: confloat(ge=50, le=90) = ...
+    lon: confloat(ge=-180, le=180) = ...
+    start_year: conint(ge=1950, le=2100) = ...
+    end_year: conint(ge=1950, le=2100) = ...
+    format: Literal["json", "csv", "netcdf", "geotiff"] = "json"
+
+
+class AnthroposphereDataParameters(BaseModel):
+    model_config = {"extra": "forbid"}
+    variable: Literal["population", "pct_minority"] = ...
+    location: Literal["AK1", "AK2", "AK3"] = ...
+    format: Literal["json", "csv", "netcdf", "geotiff"] = "json"
+
+
+@app.get("/about/")
+def root(parameters: Annotated[AboutParameters, Query()]):
+    """
+    Returns a description of the API. Optionally, returns descriptions of specific endpoints from user-specified parameters.
+    """
+    return mockup_message(parameters, "about")
+
+
+@app.get("/data/atmosphere/")
+def root(parameters: Annotated[AtmosphereDataParameters, Query()]):
+    """
+    Validates request parameters against the metadata catalog.
+    Fetches, packages, and returns atmospheric data from user-specified parameters.
+    """
+    catalog_subset = validate_parameters_against_catalog(parameters)
+    data = fetch_data_using_catalog(parameters, catalog_subset)
+    packaged_data = package_data("atmosphere", data, parameters.format)
+
+    # return packaged_data
+    return mockup_message(parameters, "atmosphere")
+
+
+@app.get("/data/hydrosphere/")
+def root(parameters: Annotated[HydrosphereDataParameters, Query()]):
+    """
+    Validates request parameters against the metadata catalog.
+    Fetches, packages, and returns hydrologic data from user-specified parameters.
+    """
+    catalog_subset = validate_parameters_against_catalog(parameters)
+    data = fetch_data_using_catalog(parameters, catalog_subset)
+    packaged_data = package_data("atmosphere", data, parameters.format)
+
+    # return packaged_data
+    return mockup_message(parameters, "hydrosphere")
+
+
+@app.get("/data/biosphere/")
+def root(parameters: Annotated[BiosphereDataParameters, Query()]):
+    """
+    Validates request parameters against the metadata catalog.
+    Fetches, packages, and returns biological data from user-specified parameters.
+    """
+    catalog_subset = validate_parameters_against_catalog(parameters)
+    data = fetch_data_using_catalog(parameters, catalog_subset)
+    packaged_data = package_data("atmosphere", data, parameters.format)
+
+    # return packaged_data
+    return mockup_message(parameters, "biosphere")
+
+
+@app.get("/data/cryosphere/")
+def root(parameters: Annotated[CryosphereDataParameters, Query()]):
+    """
+    Validates request parameters against the metadata catalog.
+    Fetches, packages, and returns cryospheric data from user-specified parameters.
+    """
+    catalog_subset = validate_parameters_against_catalog(parameters)
+    data = fetch_data_using_catalog(parameters, catalog_subset)
+    packaged_data = package_data("atmosphere", data, parameters.format)
+
+    # return packaged_data
+    return mockup_message(parameters, "cryosphere")
+
+
+@app.get("/data/anthroposphere/")
+def root(parameters: Annotated[AnthroposphereDataParameters, Query()]):
+    """
+    Validates request parameters against the metadata catalog.
+    Fetches, packages, and returns anthropological data from user-specified parameters.
+    """
+    catalog_subset = validate_parameters_against_catalog(parameters)
+    data = fetch_data_using_catalog(parameters, catalog_subset)
+    packaged_data = package_data("atmosphere", data, parameters.format)
+
+    # return packaged_data
+    return mockup_message(parameters, "anthroposphere")
---
+++ jp-fastapi/util.py
@@ -0,0 +1,64 @@
+# import a metadata catalog... this could take many formats
+catalog = {"metadata": {}}
+
+
+def validate_parameters_against_catalog(parameters, catalog=catalog):
+    """
+    Validates the parameters against the metadata catalog to ensure at least some data exists for the given parameters.
+    This function should be a simple database search to see if any data exists for combinations of the parameters.
+    Returns a subset of the catalog, which could be empty.
+    """
+    # do something to subset the catalog using parameters
+    catalog_subset = catalog
+    return catalog_subset
+
+
+def fetch_data_using_catalog(parameters, catalog_subset):
+    """
+    Fetches data using coverage info from the metadata catalog and given parameters.
+    This function will probably call other subfunctions to create WCPS queries to fetch the data.
+    """
+    # perform data fetching operations using parameters and catalog_subset
+    data = {"data": {}}
+    return data
+
+
+def package_data(service_category, data, format):
+    """
+    Packages the data into a format specified by the user.
+    Allows different packaging options for different service categories.
+    """
+    # perform data packaging operations using service_category, data, and format parameters
+    packaged_data = {"packaged_data": {}}
+    return packaged_data
+
+
+def mockup_message(parameters, route):
+    """
+    Helps to create a message for the API. This is just a mockup! The actual app will return actual data :)
+    """
+    params = list(parameters.model_fields.keys())
+    if route == "about":
+        message = f"This is a root endpoint. You have requested a general description of the API. "
+        if parameters.variable is not None:
+            message += f"You have requested more information about a specific variable ({parameters.variable}), and the variable you requested is valid."
+        return {"message": message}
+    else:
+        message = f"You have requested {route} data using the following parameters: "
+        if "variable" in params:
+            message += f"Variable: {parameters.variable} | "
+        if "source" in params:
+            message += f"Source: {parameters.source} | "
+        if "lat" in params:
+            message += f"Latitude: {parameters.lat} | "
+        if "lon" in params:
+            message += f"Longitude: {parameters.lon} | "
+        if "location" in params:
+            message += f"Location: {parameters.location} | "
+        if "start_year" in params:
+            message += f"Start Year: {parameters.start_year} | "
+        if "end_year" in params:
+            message += f"End Year: {parameters.end_year}"
+        if "format" in params:
+            message += f" | Format: {parameters.format}"
+    return {"message": message}
